a9ef23d794d529b14533911445d8301e839df60d
Yanjoo <hanjub3@gmail.com>
2022-04-04 15:43:24 +0900

    maven 프로젝트 생성

---
+++ kdt-spring-01/src/main/java/org/prgrms/App.java
@@ -0,0 +1,13 @@
+package org.prgrms;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
