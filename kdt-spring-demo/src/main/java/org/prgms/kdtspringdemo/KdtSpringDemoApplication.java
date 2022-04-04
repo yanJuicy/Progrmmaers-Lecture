00be9c9c0c9f594a357cfb54db962b041350ecdf
Yanjoo <hanjub3@gmail.com>
2022-04-04 15:59:44 +0900

    CLI 프로젝트

---
+++ kdt-spring-demo/src/main/java/org/prgms/kdtspringdemo/KdtSpringDemoApplication.java
@@ -0,0 +1,13 @@
+package org.prgms.kdtspringdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KdtSpringDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KdtSpringDemoApplication.class, args);
+	}
+
+}
