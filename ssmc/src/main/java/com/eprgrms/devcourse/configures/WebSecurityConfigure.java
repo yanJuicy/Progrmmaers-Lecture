84b3a4c6fb68ceb19b7ce207c1c29010bf43f616
yanJuicy <hanjub3@gmail.com>
2022-05-17 14:22:50 +0900

    스프링 시큐리티 day 1

---
+++ ssmc/src/main/java/com/eprgrms/devcourse/configures/WebSecurityConfigure.java
@@ -0,0 +1,35 @@
+package com.eprgrms.devcourse.configures;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfigure extends WebSecurityConfigurerAdapter{
+
+    @Override
+    public void configure(WebSecurity web) throws Exception {
+        web.ignoring().antMatchers("/assets/**");
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .authorizeRequests()
+                    .antMatchers("/me").hasAnyRole("USER", "ADMIN")
+                    .anyRequest().permitAll()
+                    .and()
+                .formLogin()
+                    .defaultSuccessUrl("/")
+                    .permitAll();
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        super.configure(auth);
+    }
+}
