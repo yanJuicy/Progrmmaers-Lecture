c166e0dad850da0352f77dc72eabd0e142812c82
yanJuicy <hanjub3@gmail.com>
2022-05-25 02:06:38 +0900

    스프링 시큐리티 day 2

--- ssmc/src/main/java/com/eprgrms/devcourse/configures/WebSecurityConfigure.java
+++ ssmc/src/main/java/com/eprgrms/devcourse/configures/WebSecurityConfigure.java
@@ -6,6 +6,7 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 @Configuration
 @EnableWebSecurity
@@ -19,17 +20,45 @@ public class WebSecurityConfigure extends WebSecurityConfigurerAdapter{
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
-                .authorizeRequests()
-                    .antMatchers("/me").hasAnyRole("USER", "ADMIN")
-                    .anyRequest().permitAll()
-                    .and()
-                .formLogin()
-                    .defaultSuccessUrl("/")
-                    .permitAll();
+            .authorizeRequests()
+                .antMatchers("/me").hasAnyRole("USER", "ADMIN")
+                .anyRequest().permitAll()
+                .and()
+            .formLogin()
+                .defaultSuccessUrl("/")
+                .permitAll()
+                .and()
+            .logout()
+                .logoutRequestMatcher(new AntPathRequestMatcher("/logout"))
+                .logoutSuccessUrl("/")
+                .invalidateHttpSession(true)
+                .clearAuthentication(true)
+                .and()
+            // remember me 설정
+            .rememberMe()
+                .rememberMeParameter("remember-me")
+                .tokenValiditySeconds(300)
+                .and()
+            // 로그아웃 설정
+            .logout()
+                .logoutRequestMatcher(new AntPathRequestMatcher("/logout"))
+                .logoutSuccessUrl("/")
+                .invalidateHttpSession(true)
+                .clearAuthentication(true)
+                .and()
+            // HTTP 요청을 HTTPS 요청으로 리다이렉트
+            .requiresChannel()
+                .anyRequest()
+                .requiresSecure();
+
+
     }
 
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        super.configure(auth);
+        auth.inMemoryAuthentication()
+                .withUser("user").password("{noop}user123").roles("USER")
+                .and()
+                .withUser("admin").password("{noop}admin123").roles("ADMIN");
     }
 }
