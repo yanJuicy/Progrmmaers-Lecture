76e802bd8b6ee7427af2ff2582a9733de56bae60
yanJuicy <hanjub3@gmail.com>
2022-05-25 02:38:42 +0900

    스프링 시큐리티 day 3

--- ssmc/src/main/java/com/eprgrms/devcourse/configures/WebSecurityConfigure.java
+++ ssmc/src/main/java/com/eprgrms/devcourse/configures/WebSecurityConfigure.java
@@ -1,20 +1,43 @@
 package com.eprgrms.devcourse.configures;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.access.AccessDeniedHandler;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+
+import javax.servlet.http.HttpServletResponse;
 
 @Configuration
 @EnableWebSecurity
 public class WebSecurityConfigure extends WebSecurityConfigurerAdapter{
 
+    private final Logger log = LoggerFactory.getLogger(getClass());
+
     @Override
     public void configure(WebSecurity web) throws Exception {
         web.ignoring().antMatchers("/assets/**");
+    }
+
+    @Bean
+    public AccessDeniedHandler accessDeniedHandler() {
+        return (accept, response, e) -> {
+            Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+            Object principal = authentication != null ? authentication.getPrincipal() : null;
+            log.warn("{} is denied", principal, e);
+            response.setStatus(HttpServletResponse.SC_FORBIDDEN);
+            response.getWriter().write("ACCESS DENIED");
+            response.getWriter().flush();
+            response.getWriter().close();
+        };
     }
 
     @Override
@@ -22,6 +45,7 @@ public class WebSecurityConfigure extends WebSecurityConfigurerAdapter{
         http
             .authorizeRequests()
                 .antMatchers("/me").hasAnyRole("USER", "ADMIN")
+                .antMatchers("/admin").access("isFullyAuthenticated() and hasRole('ADMIN')")
                 .anyRequest().permitAll()
                 .and()
             .formLogin()
@@ -49,7 +73,11 @@ public class WebSecurityConfigure extends WebSecurityConfigurerAdapter{
             // HTTP 요청을 HTTPS 요청으로 리다이렉트
             .requiresChannel()
                 .anyRequest()
-                .requiresSecure();
+                .requiresSecure()
+                .and()
+            // 예외처리 핸들러
+            .exceptionHandling()
+                .accessDeniedHandler(accessDeniedHandler());
 
 
     }
